a0ceab5f858623b0e691cd1ee17ff3fb0260231a
Asdoria <97902987+asdoria@users.noreply.github.com>
2022-05-11 10:39:00 +0200

    Update CustomerGroupsTrait.php

--- src/Traits/CustomerGroupsTrait.php
+++ src/Traits/CustomerGroupsTrait.php
@@ -7,6 +7,7 @@ namespace Asdoria\SyliusProductCustomerGroupPlugin\Traits;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Sylius\Component\Customer\Model\CustomerGroupInterface;
+use Doctrine\ORM\Mapping as ORM;
 
 trait CustomerGroupsTrait
 {
